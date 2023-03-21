2bf6bf01d2571c7c267e617db39c8599d2654af1
Ye Lwin Htay <yelwin.r.k@gmail.com>
2023-03-21 10:43:50 -0700

    flask set up

---
+++ redux-storeshape.py
@@ -0,0 +1,19 @@
+# store = {
+#     session: {},
+#     posts: {
+#         allPosts: {
+#             [postId]: { postData }
+#         },
+#         userPosts: {
+#             [postId]: { postData }
+#         }
+#     },
+#     comments: {
+#         postComments: {
+#             [commentId]: { commentData }
+#         },
+#         userComments:  {
+#             [commentId]: { commentData }
+#         }
+#     }
+# }
